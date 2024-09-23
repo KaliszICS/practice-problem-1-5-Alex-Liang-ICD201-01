a1f66b0ecfeb5a8d8b45970321334a1feea0f360
Alex-Liang-ICD201-01 <alex.liang3@student.tdsb.on.ca>
2024-09-23 14:17:52 +0000

    Commit

--- PP1_5.py
+++ PP1_5.py
@@ -2,17 +2,36 @@
 def q1():
   #Write Assignment code here
 
+  integervalue = input("Input an integer: ")
+  print(int(integervalue) + 3)
+
 def q2():
   #Write Assignment code here
+
+  q2number = input("Input a number: ")
+  print(float(q2number + "4") + 2)
 
 def q3():
   #Write Assignment code here
 
+  nradius = input("Input a radius: ")
+  print((float(nradius)) *3.14)
+
 def q4():
   #Write Assignment code here
 
+  number2 = input("Input a number: ")
+  print(int(float(number2)) * 12)
+
 def q5():
+
+  integervalue2 = input("Input an integer: ")
+  processedint2 = (int(integervalue2) + 5)
+  print(f"Your number + 5 is {processedint2}")
+
   #Write Assignment code here
+
+
 
 #Comment this code out when running tests
 #Do not comment this out when running your program normally
